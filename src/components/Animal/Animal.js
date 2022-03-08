aa988d29a29a056c8aff3011e87eb3b9bc068fdd
Andrea Cleland <andreaclelan@gmail.com>
2022-03-07 16:54:46 -0800

    got prettier set up

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './Animal.css';
 
 export default function Animal(animal) {
-  return <div className="animal" style={{ top: animal.top, left: animal.left }}>
-    <img alt={animal.name} src={`${process.env.PUBLIC_URL}/animals/${animal.type}.svg`} />
-    <span className="name">{animal.name}</span>
-    <span>{animal.name}</span>
-  </div>;
+  return (
+    <div className="animal" style={{ top: animal.top, left: animal.left }}>
+      <img alt={animal.name} src={`${process.env.PUBLIC_URL}/animals/${animal.type}.svg`} />
+      <span className="name">{animal.name}</span>
+      <span>{animal.name}</span>
+    </div>
+  );
 }
